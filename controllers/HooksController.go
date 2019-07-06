4f4fc6553597d693d42ed7fc5e2f12f25db60399
zhenlong.zhang <longy0115@163.com>
2019-07-06 22:05:40 +0800

    test

--- controllers/HooksController.go
+++ controllers/HooksController.go
@@ -1,19 +1,36 @@
 package controllers
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/astaxie/beego"
+	"log"
 	"os/exec"
 )
+
+const ShellToUse = "bash"
 
 type HooksController struct {
 	beego.Controller
 }
 
 func (c *HooksController) Get() {
-	command := "../gohooks.sh"
-	exec.Command("/bin/sh", "-c", command)
-     // 略 Other Code
-     // 永远也不可能执行到这里来
-     // bash 这条命令 (kill $pidid|go run main.go&)
-     // 会杀掉现在的进程，程序直接结束了，不可能执行后面的 go run main.go &
+	err, out, errout := Shellout("ls -ltr")
+	if err != nil {
+		log.Printf("error: %v\n", err)
+	}
+	fmt.Println("--- stdout ---")
+	fmt.Println(out)
+	fmt.Println("--- stderr ---")
+	fmt.Println(errout)
+}
+
+func Shellout(command string) (error, string, string) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(ShellToUse, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
 }
