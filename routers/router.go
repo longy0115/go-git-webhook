f53ae5419e5c1994bec4bc0f543214bf1375d6a8
zhenlong.zhang <longy0115@163.com>
2019-07-06 22:06:46 +0800

    router

--- routers/router.go
+++ routers/router.go
@@ -8,9 +8,4 @@ import (
 func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/hooks", &controllers.HooksController{})
-	
-	beego.Get("/hello",func(ctx *context.Context){
-		ctx.Output.Body([]byte("hello world"))
-	})
-
 }
